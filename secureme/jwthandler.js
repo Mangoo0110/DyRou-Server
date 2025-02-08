a3dda1dfd6f76e6e555c599a4bbe9e663d4e3f0c
Anik Saha <89010422+Mangoo0110@users.noreply.github.com>
2025-02-09 02:04:25 +0600

    Secret key in jwthandler.js has been removed.
    The secret key was manually replaced with an empty string.
    As this repo is public, the secret key should not be read by the public.

--- secureme/jwthandler.js
+++ secureme/jwthandler.js
@@ -2,7 +2,9 @@ const jwt = require('jsonwebtoken');
 
 class JwtHandler {
     constructor(secretKey) {
-        this.secretKey = "#DyRou@PasswordKey0110$";
+        // The secret key was manually replaced with an empty string.
+        // As this repo is public, the secret key should not be read by the public.
+        this.secretKey = "";
     }
     generateToken(payload, expiresIn = '3600h') {
         return jwt.sign(payload, this.secretKey);
